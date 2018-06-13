d373946e9c9cd1eb1f30b1df1cd9ef5980ae2bc9
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-12 19:26:56 -0700

    inorder with parent pointers

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
 
 
-def inorder_traversal(tree):
-    # TODO - you fill in here.
-    return []
+def inorder_traversal(root):
+    return list(it(root))
+
+
+def it(root):
+    '''In-order traversal in a tree with parent pointers.
+
+    T(n) = O(n)
+    S(n) = O(1)
+    '''
+    prev = None
+    while root:
+        if prev is root.parent:
+            while root.left:
+                root = root.left
+            yield root.data
+            prev, root = root, root.right or root.parent
+        elif prev is root.left:
+            yield root.data
+            prev, root = root, root.right or root.parent
+        else:
+            prev, root = root, root.parent
 
 
 if __name__ == '__main__':
