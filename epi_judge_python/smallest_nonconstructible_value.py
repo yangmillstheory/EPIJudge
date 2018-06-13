d4822b22e538f413344ffc6aa56f427713f8eed0
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-12 20:17:53 -0700

    smallest non-constructible value

--- epi_judge_python/smallest_nonconstructible_value.py
+++ epi_judge_python/smallest_nonconstructible_value.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
 
-def smallest_nonconstructible_value(A):
-    # TODO - you fill in here.
-    return 0
+def smallest_nonconstructible_value(xs):
+    '''Finds the smallest poz. integer that's not
+    a subset sum of xs. xs is assumed to be sorted.
+
+    O(n) time
+    O(1) space
+    '''
+    xs.sort()
+    out = 1
+    for x in xs:
+        if x <= out:
+            out += x
+        else:
+            break
+    return out
 
 
 if __name__ == '__main__':
