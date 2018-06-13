b8990b0e37a142486d31289d6a71334e88c20bf7
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-12 19:39:49 -0700

    linked list from leaves

--- epi_judge_python/tree_connect_leaves.py
+++ epi_judge_python/tree_connect_leaves.py
@@ -1,13 +1,43 @@
 import functools
 
+from collections import deque
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def create_list_of_leaves(tree):
-    # TODO - you fill in here.
-    return []
+class ListNode:
+    def __init__(self, data, node):
+        self.data = data
+        self.next = node
+
+
+def _iter_preorder(root):
+    stack = deque()
+    while root or stack:
+        if root:
+            if root.right:
+                stack.append(root.right)
+            yield root
+            root = root.left
+        else:
+            root = stack.pop()
+
+
+def _iter_leaves_preorder(root):
+    for node in _iter_preorder(root):
+        if not node.left and not node.right:
+            yield node
+
+
+def create_list_of_leaves(root):
+    # represent a linked list node as a tuple
+    res, node = [], ListNode(0, None)
+    for leaf in _iter_leaves_preorder(root):
+        node.next = ListNode(leaf.data, None)
+        res.append(node.next)
+        node = node.next
+    return res
 
 
 @enable_executor_hook
