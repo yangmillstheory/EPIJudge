8d58727369f25a1c1f2a9568d4a06e0bec79c3a9
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-12 20:49:56 -0700

    binary tree exterior

--- epi_judge_python/tree_exterior.py
+++ epi_judge_python/tree_exterior.py
@@ -1,13 +1,56 @@
 import functools
+import collections
 
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def exterior_binary_tree(tree):
-    # TODO - you fill in here.
-    return []
+def l_side(root, res):
+    if not root:
+        return
+    if _is_leaf(root):
+        return
+    res.append(root)
+    if root.left:
+        l_side(root.left, res)
+    else:
+        l_side(root.right, res)
+
+
+def leaves(root, orig, res):
+    if not root:
+        return
+    if _is_leaf(root) and root is not orig:
+        res.append(root)
+    leaves(root.left, orig, res)
+    leaves(root.right, orig, res)
+
+
+def r_side(root, res):
+    if not root or _is_leaf(root):
+        return
+    if root.right:
+        r_side(root.right, res)
+    else:
+        r_side(root.left, res)
+    res.append(root)
+
+
+def _is_leaf(root):
+    return not root.right and not root.left
+
+
+def exterior_binary_tree(root):
+    if not root:
+        return []
+    res = [root]
+    if _is_leaf(root):
+        return res
+    l_side(root.left, res)
+    leaves(root, root, res)
+    r_side(root.right, res)
+    return res
 
 
 def create_output_list(L):
