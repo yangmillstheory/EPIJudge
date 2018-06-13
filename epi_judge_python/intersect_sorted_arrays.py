fda66aa3aa578391feb851129785e2d855d1e7b7
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-12 20:16:54 -0700

    intersect 2 arrays

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
+def intersect_two_sorted_arrays(a, b):
+    """Returns the intersection of two sorted lists
+    in O(m + n) time.
 
-def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
+    """
+    m, n = len(a), len(b)
+    i = j = 0
+    res = []
+    while i < m and j < n:
+        if a[i] > b[-1] or b[j] > a[-1]:
+            break
+        elif a[i] == b[j]:
+            if not res or res[-1] != a[i]:
+                res.append(a[i])
+            i += 1
+            j += 1
+        elif a[i] < b[j]:
+            i += 1
+        else:
+            j += 1
+    return res
 
 
 if __name__ == '__main__':
