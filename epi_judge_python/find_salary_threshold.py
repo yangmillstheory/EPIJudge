1c84f6a69f0c4febb6e0ad9380bc0124f36b94c8
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-12 20:18:39 -0700

    find salary cap

--- epi_judge_python/find_salary_threshold.py
+++ epi_judge_python/find_salary_threshold.py
@@ -1,9 +1,16 @@
 from test_framework import generic_test
 
 
-def find_salary_cap(target_payroll, current_salaries):
-    # TODO - you fill in here.
-    return 0.0
+def find_salary_cap(t, xs):
+    xs.sort()
+    n = len(xs)
+    raw = 0
+    for j, x in enumerate(xs):
+        adj = (n-j)*x
+        if raw + adj >= t:
+            return (t-raw)/(n-j)
+        raw += x
+    return -1
 
 
 if __name__ == '__main__':
