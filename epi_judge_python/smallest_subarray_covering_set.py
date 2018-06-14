192198fd6fd1539b11bcc9ef12d5db0585527c40
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-14 08:13:20 -0700

    smallest subarray covering set

--- epi_judge_python/smallest_subarray_covering_set.py
+++ epi_judge_python/smallest_subarray_covering_set.py
@@ -8,9 +8,33 @@ from test_framework.test_utils import enable_executor_hook
 Subarray = collections.namedtuple('Subarray', ('start', 'end'))
 
 
-def find_smallest_subarray_covering_set(paragraph, keywords):
-    # TODO - you fill in here.
-    return Subarray(0, 0)
+def find_smallest_subarray_covering_set(text, pat):
+    '''Find the smallest subarray covering a set of words.
+
+        T(t, p) = O(t)
+        S(t, p) = O(p)
+    '''
+    n, j = len(text), 0
+    counts, rem, sub = collections.Counter(), set(pat), None
+    for i in range(n):
+        if rem:
+            # expand right to cover pat
+            while j < n and rem:
+                ch = text[j]
+                if ch in pat:
+                    counts[ch] += 1
+                if ch in rem:
+                    rem.remove(ch)
+                j += 1
+        if not rem and (not sub or j-1-i < sub.end-sub.start):
+            sub = Subarray(i, j-1)
+        ch = text[i]
+        if ch in counts:
+            counts[ch] -= 1
+            if not counts[ch]:
+                rem.add(ch)
+                del counts[ch]
+    return sub
 
 
 @enable_executor_hook
