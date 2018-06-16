9eba6cde63b7d332387a61372d9ff22c61e1da2a
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-16 13:15:33 -0700

    WIP

--- epi_judge_python/string_decompositions_into_dictionary_words.py
+++ epi_judge_python/string_decompositions_into_dictionary_words.py
@@ -1,9 +1,35 @@
+from collections import Counter
 from test_framework import generic_test
 
 
-def find_all_substrings(s, words):
-    # TODO - you fill in here.
-    return []
+def find_all_substrings(string, words):
+    res = []
+    n_words = len(words)
+    if not n_words:
+        return res
+    n_word = len(words[0])
+    orig = set(words)
+    n = len(string)
+    bad = set()
+    for i in range(n-(n_words*n_word)+1):
+        if i in bad:
+            continue
+        rem = Counter(words)
+        for j in range(i, n-n_word+1, n_word):
+            sub = string[j:j+n_word]
+            if sub not in orig:
+                bad.add(j)
+                break
+            if sub in rem:
+                rem[sub] -= 1
+                if not rem[sub]:
+                    del rem[sub]
+                if not rem:
+                    res.append(i)
+                    break
+            else:
+                break
+    return res
 
 
 if __name__ == '__main__':
