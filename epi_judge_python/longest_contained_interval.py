7ce79a8be7bd0665e282fe9b5c246dc3013b4e2f
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-16 22:08:58 -0700

    a simple set-based problem

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -1,9 +1,22 @@
 from test_framework import generic_test
 
 
-def longest_contained_range(A):
-    # TODO - you fill in here.
-    return 0
+def longest_contained_range(a):
+    '''Find the longest contained range in a list of
+    numbers in O(n) time and O(n) space.'''
+    max_len = 0
+    rem, a = set(a), set(a)
+    while rem:
+        curr = rem.pop()
+        n, l, r = 1, curr-1, curr+1
+        while l in a:
+            rem.remove(l)
+            n, l = n+1, l-1
+        while r in a:
+            rem.remove(r)
+            n, r = n+1, r+1
+        max_len = max(max_len, n)
+    return max_len
 
 
 if __name__ == '__main__':
