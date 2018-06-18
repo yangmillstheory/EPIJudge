172e6a27e4e5555056b292a2c10d076dc7a8c9a8
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-17 22:16:11 -0700

    classical DP

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
 
 
-def num_combinations_for_final_score(final_score, individual_play_scores):
-    # TODO - you fill in here.
-    return 0
+def num_combinations_for_final_score(score, plays):
+    # T(a, c) = O(a*c)
+    # S(a, c) = O(c)
+    if score == 0:
+        return 1
+    if not plays:
+        return 0
+    n_plays = len(plays)
+    prev = [1]+([0]*score)
+    curr, init = [1]+([0]*score), True
+    for i in range(n_plays):
+        if not init:
+            curr = [1]+([0]*score)
+        for j in range(1, score+1):
+            incl_c_i = curr[j-plays[i]] if j-plays[i] >= 0 else 0
+            excl_c_i = prev[j] if i-1 >= 0 else 0
+            curr[j] = incl_c_i+excl_c_i
+        prev, init = curr, False
+    return curr[-1]
 
 
 if __name__ == '__main__':
