8ff638e7a15ca960d9d3370015ed0096dd2e23ef
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-18 20:11:58 -0700

    simple DP

--- epi_judge_python/minimum_weight_path_in_a_triangle.py
+++ epi_judge_python/minimum_weight_path_in_a_triangle.py
@@ -1,9 +1,24 @@
 from test_framework import generic_test
 
 
-def minimum_path_weight(triangle):
-    # TODO - you fill in here.
-    return 0
+def minimum_path_weight(tri):
+    # T(m,n) = O(m*n)
+    # S(m,n) = O(n)
+    if not tri:
+        return 0
+    m, prev = len(tri), [tri[0][0]]
+    for i in range(1, m):
+        n = len(tri[i])
+        curr = [None]*n
+        for j in range(n):
+            cand = []
+            if j > 0:
+                cand.append(tri[i][j]+prev[j-1])
+            if j < n-1:
+                cand.append(tri[i][j]+prev[j])
+            curr[j] = min(cand)
+        prev = curr
+    return min(prev)
 
 
 if __name__ == '__main__':
