6606254e25e5bbbc6500ab461a918793926b47b3
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-22 20:50:26 -0700

    add some pruning!

--- epi_judge_python/is_string_decomposable_into_words.py
+++ epi_judge_python/is_string_decomposable_into_words.py
@@ -5,9 +5,29 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def decompose_into_dictionary_words(domain, dictionary):
-    # TODO - you fill in here.
-    return []
+def _walk(s, words, start, used, seen):
+    if start == len(s):
+        return True, used
+    if start in seen:
+        return False, used
+    for i in range(start+1, len(s)+1):
+        if i in seen:
+            continue
+        sub = s[start:i]
+        if sub in words:
+            used.append(sub)
+            if _walk(s, words, i, used, seen)[0]:
+                return True, used
+            used.pop()
+    seen.add(start)
+    return False, used
+
+
+def decompose_into_dictionary_words(s, words):
+    ok, used = _walk(s, words, 0, [], set())
+    if ok:
+        return used
+    return False
 
 
 @enable_executor_hook
