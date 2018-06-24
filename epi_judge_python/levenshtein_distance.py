278ab09118b8cc5dedafbb75b8e80c7e107c6e11
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-24 13:25:34 -0700

    first DP solution for edit distance

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,9 +1,22 @@
 from test_framework import generic_test
 
 
-def levenshtein_distance(A, B):
-    # TODO - you fill in here.
-    return 0
+def levenshtein_distance(a, b):
+    # T(m,n) = S(m,n) = O(m*n)
+    m, n = len(a), len(b)
+    dp = [[None]*(n+1) for _ in range(m+1)]
+    for j in range(n+1):
+        dp[0][j] = j
+    for i in range(m+1):
+        dp[i][0] = i
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            dp[i][j] = min(
+                dp[i-1][j-1]+int(a[i-1] != b[j-1]),
+                dp[i-1][j] + 1,
+                dp[i][j-1] + 1,
+            )
+    return dp[-1][-1]
 
 
 if __name__ == '__main__':
