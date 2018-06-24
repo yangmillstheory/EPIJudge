01005da8838a41a3493a64561099d134876db68e
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-24 13:42:04 -0700

    optimize space

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,7 +1,7 @@
 from test_framework import generic_test
 
 
-def levenshtein_distance(a, b):
+def _dp_1(a, b):
     # T(m,n) = S(m,n) = O(m*n)
     m, n = len(a), len(b)
     dp = [[None]*(n+1) for _ in range(m+1)]
@@ -17,6 +17,24 @@ def levenshtein_distance(a, b):
                 dp[i][j-1] + 1,
             )
     return dp[-1][-1]
+
+
+def levenshtein_distance(a, b):
+    # T(m,n) = O(m*n)
+    # S(m,n) = O(n)
+    m, n = len(a), len(b)
+    dp = list(range(n+1))
+    for i in range(1, m+1):
+        prev_j_minus_1, dp[0] = dp[0], i
+        for j in range(1, n+1):
+            temp = dp[j]
+            dp[j] = min(
+                prev_j_minus_1+int(a[i-1] != b[j-1]),
+                dp[j-1]+1,
+                dp[j]+1,
+            )
+            prev_j_minus_1 = temp
+    return dp[-1]
 
 
 if __name__ == '__main__':
