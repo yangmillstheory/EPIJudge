6a87eb614cc88a8c471c8499dc9313b43f0ad891
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-24 15:58:07 -0700

    choose recursive implementation

--- epi_judge_python/picking_up_coins.py
+++ epi_judge_python/picking_up_coins.py
@@ -2,8 +2,22 @@ from test_framework import generic_test
 
 
 def maximum_revenue(coins):
-    # TODO - you fill in here.
-    return 0
+    # note that it's harder to do this in a tabular way
+    # due to the order that the subproblems are computed
+    n = len(coins)
+    dp = [[0]*n for _ in range(n)]
+
+    def compute_coin_range(i, j):
+        if i > j:
+            return 0
+        if dp[i][j] != 0:
+            return dp[i][j]
+        dp[i][j] = max(
+            coins[i] + min(compute_coin_range(i+1, j-1), compute_coin_range(i+2, j)),
+            coins[j] + min(compute_coin_range(i+1, j-1), compute_coin_range(i, j-2))
+        )
+        return dp[i][j]
+    return compute_coin_range(0, n-1)
 
 
 if __name__ == '__main__':
