77616f4baf6eff3d41909aa8be69ff693e1c6d99
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-25 18:08:07 -0700

    minimum waiting time

--- epi_judge_python/minimum_waiting_time.py
+++ epi_judge_python/minimum_waiting_time.py
@@ -2,8 +2,11 @@ from test_framework import generic_test
 
 
 def minimum_total_waiting_time(service_times):
-    # TODO - you fill in here.
-    return 0
+    service_times.sort()
+    res, n = 0, len(service_times)
+    for j, service_time in enumerate(service_times, 1):
+        res += (n-j) * service_time
+    return res
 
 
 if __name__ == '__main__':
