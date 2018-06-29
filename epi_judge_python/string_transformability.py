842866969c9a01f407524db210d27293ed299a83
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-28 21:57:11 -0700

    some optimization

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -3,7 +3,13 @@ from test_framework import generic_test
 
 
 def transform_string(words, s, t):
-    alpha = {ch for word in words for ch in word}
+    def get_alpha():
+        alpha = collections.defaultdict(set)
+        for word in words:
+            for i, ch in enumerate(word):
+                alpha[i].add(ch)
+        return alpha
+    alpha = get_alpha()
     q, seen = collections.deque([(s, 0)]), set()
     while q:
         v, n = q.popleft()
@@ -11,7 +17,7 @@ def transform_string(words, s, t):
             return n
         chars = list(v)
         for i, ch in enumerate(v):
-            for _ch in alpha:
+            for _ch in alpha[i]:
                 chars[i] = _ch
                 u = ''.join(chars)
                 if u in words and u not in seen and u != v:
