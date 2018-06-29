b3d9cfa6cd05b1dadb4ffeb589378a3234c46658
Victor Alvarez <v.alvarez312@gmail.com>
2018-06-28 21:48:19 -0700

    a bfs problem

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,10 +1,25 @@
+import collections
 from test_framework import generic_test
 
 
-# Uses BFS to find the least steps of transformation.
-def transform_string(D, s, t):
-    # TODO - you fill in here.
-    return 0
+def transform_string(words, s, t):
+    alpha = {ch for word in words for ch in word}
+    q, seen = collections.deque([(s, 0)]), set()
+    while q:
+        v, n = q.popleft()
+        if v == t:
+            return n
+        chars = list(v)
+        for i, ch in enumerate(v):
+            for _ch in alpha:
+                chars[i] = _ch
+                u = ''.join(chars)
+                if u in words and u not in seen and u != v:
+                    q.append((u, n+1))
+                seen.add(u)
+            chars[i] = ch
+        seen.add(v)
+    return -1
 
 
 if __name__ == '__main__':
