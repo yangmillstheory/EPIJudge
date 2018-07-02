7905a580d7d330976f86ba2631bd71a4fee3da5d
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-02 13:31:41 -0700

    RPN stuff

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,34 @@
 from test_framework import generic_test
 
 
-def evaluate(expression):
-    # TODO - you fill in here.
-    return 0
+ops = {'+', '-', '/', '*'}
+
+
+def signum(x):
+    return -1 if x < 0 else 1
+
+
+def evaluate(tokens):
+    # T(n) = S(n) = O(n)
+    tokens = tokens.split(',')
+    nums = []
+    for ch in tokens:
+        if ch not in ops:
+            nums.append(int(ch))
+            continue
+        b, a = nums.pop(), nums.pop()
+        if ch == '+':
+            c = a+b
+        elif ch == '-':
+            c = a-b
+        elif ch == '/':
+            c = signum(a)*signum(b)*(abs(a)//abs(b))
+        elif ch == '*':
+            c = a*b
+        else:
+            raise ValueError(ch)
+        nums.append(c)
+    return nums.pop()
 
 
 if __name__ == '__main__':
