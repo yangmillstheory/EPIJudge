763f36c38d03a00bbb7ce45bffb9ee3b60c5f6a0
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-01 20:57:24 -0700

    fucking hard invariant problem

--- epi_judge_python/largest_rectangle_under_skyline.py
+++ epi_judge_python/largest_rectangle_under_skyline.py
@@ -1,9 +1,21 @@
+from collections import namedtuple
 from test_framework import generic_test
 
+Pillar = namedtuple('Pillar', ['i', 'h'])
 
-def calculate_largest_rectangle(heights):
-    # TODO - you fill in here.
-    return 0
+
+def calculate_largest_rectangle(hs):
+    pillars, res = [], 0
+    for i, h in enumerate(hs + [0]):
+        while pillars and pillars[-1].h > h:
+            _i, _h = pillars.pop()
+            if pillars:
+                w = i - pillars[-1].i - 1
+            else:
+                w = i
+            res = max(res, w*_h)
+        pillars.append(Pillar(i, h))
+    return res
 
 
 if __name__ == '__main__':
