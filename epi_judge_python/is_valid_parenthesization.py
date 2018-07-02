4f79b86799e8f2c2ff672c26c733a41a7f3ebad7
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-01 21:50:37 -0700

    valid parens

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -2,9 +2,20 @@ from test_framework import generic_test
 
 
 def is_well_formed(s):
-    # TODO - you fill in here.
-    return True
-
+    '''T(n) = S(n) = O(n)'''
+    opens_rem = []
+    close_to_open = {'}': '{', ')': '(', ']': '['}
+    opens = set(close_to_open.values())
+    for ch in s:
+        if ch in opens:
+            opens_rem.append(ch)
+        elif ch in close_to_open:
+            if not opens_rem or opens_rem[-1] != close_to_open[ch]:
+                return False
+            opens_rem.pop()
+        else:
+            raise ValueError(ch)
+    return not opens_rem
 
 if __name__ == '__main__':
     exit(
