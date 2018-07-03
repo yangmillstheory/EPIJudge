6b6b17f629c0d7713411e3f805aec0d62aed87e1
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-02 20:05:39 -0700

    messy solution

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,9 +1,42 @@
 from test_framework import generic_test
 
 
+DELIM = '/'
+
+
+def append_if_nonempty(l, x):
+    if x:
+        l.append(x)
+
+
 def shortest_equivalent_path(path):
-    # TODO - you fill in here.
-    return ''
+    # T(n) = S(n) = O(n)
+    nodes, cur = [], ''
+    i, n = 0, len(path)
+    while i < n:
+        ch = path[i]
+        if ch == DELIM:
+            # eat up the extra delimiters
+            j = i
+            while j < n and path[j] == DELIM:
+                j += 1
+            append_if_nonempty(nodes, cur)
+            cur, i = '', j
+        else:
+            cur += ch
+            i += 1
+    append_if_nonempty(nodes, cur)
+    res = []
+    for node in nodes:
+        if node == '..':
+            if res and res[-1] != '..':
+                res.pop()
+            else:
+                res.append(node)
+        elif node != '.':
+            res.append(node)
+    res = DELIM.join(res)
+    return DELIM+res if path.startswith(DELIM) else res
 
 
 if __name__ == '__main__':
