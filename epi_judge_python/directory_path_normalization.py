d0a5313315ef2dd5e64863bc011eba2deb2b9db0
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-02 20:22:29 -0700

    cleaner

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -7,9 +7,7 @@ DELIM = '/'
 def shortest_equivalent_path(path):
     # T(n) = S(n) = O(n)
     nodes = []
-    for node in path.split(DELIM):
-        if node == '.' or node == '':
-            continue
+    for node in filter(lambda node: node and node != '.', path.split(DELIM)):
         if node == '..':
             if not nodes or nodes[-1] == '..':
                 nodes.append(node)
