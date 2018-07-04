b3951b3b836e2bd25db715db48d857ad6c950d4a
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-04 16:19:00 -0700

    BFS

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -1,9 +1,26 @@
+from collections import deque, namedtuple
 from test_framework import generic_test
 
 
-def binary_tree_depth_order(tree):
-    # TODO - you fill in here.
-    return []
+NodeWithDepth = namedtuple('NodeWithDepth', ['node', 'depth'])
+
+
+def binary_tree_depth_order(root):
+    # T(n) = S(n) = O(n)
+    levels = []
+    if not root:
+        return levels
+    q = deque([NodeWithDepth(root, 0)])
+    while q:
+        node, depth = q.popleft()
+        if len(levels) == depth:
+            levels.append([])
+        levels[depth].append(node.data)
+        if node.left:
+            q.append(NodeWithDepth(node.left, depth+1))
+        if node.right:
+            q.append(NodeWithDepth(node.right, depth+1))
+    return levels
 
 
 if __name__ == '__main__':
