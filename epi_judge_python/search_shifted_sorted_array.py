392b54082150640d8a9aef4fc2f4f945cf8622ea
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-05 16:03:27 -0700

    search min. in rotated sorted array

--- epi_judge_python/search_shifted_sorted_array.py
+++ epi_judge_python/search_shifted_sorted_array.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
 
 
-def search_smallest(A):
-    # TODO - you fill in here.
-    return 0
+def search_smallest(a):
+    # T(n) = O(log n)
+    # S(n) = O(1)
+    lo, hi = 0, len(a)-1
+    while lo <= hi:
+        mid = lo + (hi-lo)//2
+        if a[mid] > a[hi]:
+            lo = mid+1
+        elif mid == 0 or (mid-1 >= 0 and a[mid-1] > a[mid]):
+            return mid
+        else:
+            hi = mid-1
+    raise RuntimeError
 
 
 if __name__ == '__main__':
