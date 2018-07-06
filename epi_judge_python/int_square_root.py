7ef2fde6af4487ecfc88537dd0952b772fde7379
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-05 20:41:40 -0700

    log time sq. root

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -11,8 +11,21 @@ def _linear_time_sq_root(k):
     return cand
 
 
+def _log_time_sq_root(k):
+    lo, hi = 0, k
+    cand = 0
+    while lo <= hi:
+        mid = lo + (hi-lo)//2
+        if pow(mid, 2) <= k:
+            cand, lo = mid, mid+1
+        else:
+            hi = mid-1
+    return cand
+
+
 def square_root(k):
-    return _linear_time_sq_root(k)
+    # return _linear_time_sq_root(k)
+    return _log_time_sq_root(k)
 
 
 if __name__ == '__main__':
