a731e5245d5294326ad26ce3ca6fd34238864013
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-07 15:00:18 -0700

    k largest elements in a BST

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -1,9 +1,21 @@
+import itertools
 from test_framework import generic_test, test_utils
 
 
+def rev_order(tree):
+    stack = []
+    while stack or tree:
+        if tree:
+            stack.append(tree)
+            tree = tree.right
+        else:
+            tree = stack.pop()
+            yield tree.data
+            tree = tree.left
+
+
 def find_k_largest_in_bst(tree, k):
-    # TODO - you fill in here.
-    return None
+    return [x for x in itertools.islice(rev_order(tree), k)]
 
 
 if __name__ == '__main__':
