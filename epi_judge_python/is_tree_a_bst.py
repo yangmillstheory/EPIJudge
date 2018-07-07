8653c92b6eec115e5d08710068518eeda5c95835
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-06 18:29:27 -0700

    BST validation (inorder)

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,14 +1,33 @@
 from test_framework import generic_test
 
 
-def is_binary_tree_bst(tree, key_min=float('-inf'), key_max=float('inf')):
+def _preorder(tree, key_min=float('-inf'), key_max=float('inf')):
     # T(n) = S(n) = O(n)
     if not tree:
         return True
     l, r, data = tree.left, tree.right, tree.data
     if data < key_min or data > key_max:
         return False
-    return is_binary_tree_bst(l, key_min, min(key_max, data)) and is_binary_tree_bst(r, max(key_min, data), key_max)
+    return _preorder(l, key_min, min(key_max, data)) and _preorder(r, max(key_min, data), key_max)
+
+
+def _inorder(tree):
+    stack, prev = [], float('-inf')
+    while stack or tree:
+        if tree:
+            stack.append(tree)
+            tree = tree.left
+        else:
+            tree = stack.pop()
+            if tree.data < prev:
+                return False
+            prev = tree.data
+            tree = tree.right
+    return True
+
+
+def is_binary_tree_bst(tree):
+    return _inorder(tree)
 
 
 if __name__ == '__main__':
