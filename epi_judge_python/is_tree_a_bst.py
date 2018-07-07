4cd5ddd6176868b2b0e0f8740f7f8736e4fd043c
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-06 18:08:04 -0700

    BST validation

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,9 +1,14 @@
 from test_framework import generic_test
 
 
-def is_binary_tree_bst(tree, low_range=float('-inf'), high_range=float('inf')):
-    # TODO - you fill in here.
-    return True
+def is_binary_tree_bst(tree, key_min=float('-inf'), key_max=float('inf')):
+    # T(n) = S(n) = O(n)
+    if not tree:
+        return True
+    l, r, data = tree.left, tree.right, tree.data
+    if data < key_min or data > key_max:
+        return False
+    return is_binary_tree_bst(l, key_min, min(key_max, data)) and is_binary_tree_bst(r, max(key_min, data), key_max)
 
 
 if __name__ == '__main__':
