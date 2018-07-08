7057f1b120fbba8178351eb813f8e70f97e0bf3e
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-07 18:10:20 -0700

    BST from preorder

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -1,9 +1,25 @@
+import collections
+from bst_node import BstNode
 from test_framework import generic_test
 
 
-def rebuild_bst_from_preorder(preorder_sequence):
-    # TODO - you fill in here.
-    return None
+def _reconstruct(q, data_min=float('-inf'), data_max=float('inf')):
+    if not q:
+        return
+    data = q[0]
+    if data < data_min or data > data_max:
+        return
+    q.popleft()
+    return BstNode(
+        data,
+        _reconstruct(q, data_min, min(data_max, data)),
+        _reconstruct(q, max(data_min, data), data_max)
+    )
+
+
+def rebuild_bst_from_preorder(seq):
+    # T(n) = S(n) = O(n)
+    return _reconstruct(collections.deque(seq))
 
 
 if __name__ == '__main__':
