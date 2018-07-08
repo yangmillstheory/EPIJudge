7fd1511ce14090c12d07e391438cf0bf17258e77
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-08 11:53:57 -0700

    range lookup problem

--- epi_judge_python/range_lookup_in_bst.py
+++ epi_judge_python/range_lookup_in_bst.py
@@ -5,9 +5,50 @@ from test_framework import generic_test
 Interval = collections.namedtuple('Interval', ('left', 'right'))
 
 
+def floor(tree, key):
+    cand = None
+    while tree:
+        if tree.data <= key:
+            cand, tree = tree, tree.right
+        else:
+            tree = tree.left
+    return cand
+
+
+def ceiling(tree, key):
+    cand = None
+    while tree:
+        if tree.data >= key:
+            cand, tree = tree, tree.left
+        else:
+            tree = tree.right
+    return cand
+
+
+def successor(tree, node):
+    cand = None
+    while tree:
+        if tree.data > node.data:
+            cand, tree = tree, tree.left
+        else:
+            tree = tree.right
+    return cand
+
+
 def range_lookup_in_bst(tree, interval):
-    # TODO - you fill in here.
-    return []
+    res = []
+    if not tree:
+        return res
+    a, b = interval
+    x, y = ceiling(tree, a), floor(tree, b)
+    if not x or not y:
+        return res
+    while x and a <= x.data <= b:
+        res.append(x.data)
+        if x == y:
+            break
+        x = successor(tree, x)
+    return res
 
 
 def range_lookup_in_bst_wrapper(tree, i):
