3ebed57dcaee07a6dfb9cd9c3cf32ad7233b2bb5
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-07 18:52:45 -0700

    recusive min-height BST building

--- epi_judge_python/bst_from_sorted_array.py
+++ epi_judge_python/bst_from_sorted_array.py
@@ -1,5 +1,5 @@
 import functools
-
+from bst_node import BstNode
 from test_framework import generic_test
 from test_framework.binary_tree_utils import (binary_tree_height,
                                               generate_inorder)
@@ -7,9 +7,19 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def build_min_height_bst_from_sorted_array(A):
-    # TODO - you fill in here.
-    return None
+def _min_height_bst(a, lo, hi):
+    if lo > hi:
+        return
+    mid = lo + (hi-lo)//2
+    tree = BstNode(a[mid])
+    tree.left, tree.right = _min_height_bst(a, lo, mid-1), _min_height_bst(a, mid+1, hi)
+    return tree
+
+
+def build_min_height_bst_from_sorted_array(a):
+    # T(n) = O(n)
+    # S(n) = O(log n)
+    return _min_height_bst(a, 0, len(a)-1)
 
 
 @enable_executor_hook
