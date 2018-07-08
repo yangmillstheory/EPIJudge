1f8d6f7ebb1263709f05e26278ed8c6c20d9a549
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-07 17:06:15 -0700

    LCA in BST (iterative)

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -7,21 +7,33 @@ from test_framework.test_utils import enable_executor_hook
 
 
 # Input nodes are nonempty and the key at s is less than or equal to that at b.
-def find_LCA(tree, x, y):
+def _find_lca_recursive(tree, x, y):
     # T(n) = S(n) = O(h)
     if not tree:
         return
     if max(x.data, y.data) < tree.data:
-        return find_LCA(tree.left, x, y)
+        return _find_lca_recursive(tree.left, x, y)
     if min(x.data, y.data) > tree.data:
-        return find_LCA(tree.right, x, y)
+        return _find_lca_recursive(tree.right, x, y)
+    return tree
+
+
+def find_LCA(tree, x, y):
+    while tree:
+        data, l, r = tree.left, tree.right, tree.data
+        if max(x.data, y.data) < data:
+            tree = l
+        if min(x.data, y.data) > data:
+            tree = r
+        else:
+            break
     return tree
 
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, s, b):
     result = executor.run(
-        functools.partial(find_LCA, tree, must_find_node(tree, s),
+        functools.partial(_find_lca_recursive, tree, must_find_node(tree, s),
                           must_find_node(tree, b)))
     if result is None:
         raise TestFailure("Result can't be None")
