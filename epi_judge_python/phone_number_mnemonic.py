867d90f3e62e1b8bc914d04331645f66682e88db
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-10 07:38:32 -0700

    phone number

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -1,9 +1,35 @@
 from test_framework import generic_test, test_utils
 
 
-def phone_mnemonic(phone_number):
-    # TODO - you fill in here.
-    return []
+keypad = {
+    '0': '0',
+    '1': '1',
+    '2': 'ABC',
+    '3': 'DEF',
+    '4': 'GHI',
+    '5': 'JKL',
+    '6': 'MNO',
+    '7': 'PQRS',
+    '8': 'TUV',
+    '9': 'WXYZ',
+}
+
+
+def phone_mnemonic(s, i=0, cand=None, res=None):
+    # T(n) = O(4^n)
+    # S(n) = O(n)
+    if res is None:
+        res = []
+    if cand is None:
+        cand = []
+    if i == len(s):
+        res.append(''.join(cand))
+    else:
+        for ch in keypad[s[i]]:
+            cand.append(ch)
+            phone_mnemonic(s, i+1, cand, res)
+            cand.pop()
+    return res
 
 
 if __name__ == '__main__':
