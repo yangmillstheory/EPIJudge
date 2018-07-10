9ee9f4e85544a281831e04f0b60a2ddcbdf820bc
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-09 21:44:53 -0700

    str2int and int2str

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,31 @@
+from collections import deque
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x):
-    # TODO - you fill in here.
-    return ''
+    if x == 0:
+        return '0'
+    is_neg = x < 0
+    buf, x, base = deque(), abs(x), 10
+    while x:
+        x, rem = divmod(x, base)
+        buf.appendleft(str(rem))
+    if is_neg:
+        buf.appendleft('-')
+    return ''.join(buf)
 
 
 def string_to_int(s):
-    # TODO - you fill in here.
-    return 0
+    if s == '0':
+        return 0
+    is_neg = s and s[0] == '-'
+    if is_neg:
+        s = s[1:]
+    res, base = 0, 10
+    for i, ch in enumerate(reversed(s)):
+        res += pow(base, i)*int(ch)
+    return -res if is_neg else res
 
 
 def wrapper(x, s):
