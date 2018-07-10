924198e66e3fdf8bb2c6e3034759dd080a00ae91
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-09 20:20:43 -0700

    fuck this problem

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,9 +1,34 @@
+from collections import deque
 from test_framework import generic_test
 
 
-def convert_base(num_as_string, b1, b2):
-    # TODO - you fill in here.
-    return ''
+def to_base_10(s, b):
+    '''Convert a string in base b <= 16 to a base 10 integer.'''
+    res = 0
+    for i, c in enumerate(reversed(s)):
+        res += pow(b, i)*(int(c) if c.isdigit() else ord(c)-ord('A')+10)
+    return res
+
+
+def from_base_10(x, b):
+    buf = deque()
+    while x:
+        x, rem = divmod(x, b)
+        buf.appendleft(str(rem) if rem < 10 else chr(ord('A')+rem-10))
+        if x == -1:
+            break
+    return ''.join(buf)
+
+
+def convert_base(s, b1, b2):
+    is_neg = s and s[0] == '-'
+    if is_neg:
+        s = s[1:]
+    if s == '0':
+        res = '0'
+    else:
+        res = from_base_10(to_base_10(s, b1), b2)
+    return '-' + res if is_neg else res
 
 
 if __name__ == '__main__':
