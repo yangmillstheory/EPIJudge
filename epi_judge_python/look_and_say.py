1900e92546a7769e6967a2dd4a2b3dda5685e083
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-11 19:51:42 -0700

    toy problem

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,9 +1,27 @@
+import collections
 from test_framework import generic_test
+
+_sentinel = object()
 
 
 def look_and_say(n):
-    # TODO - you fill in here.
-    return ''
+    res = collections.deque([1, _sentinel])
+    for _ in range(1, n):
+        count, prev = 0, None
+        while res:
+            x = res.popleft()
+            if prev is None or x == prev:
+                count += 1
+            else:
+                res.append(count)
+                res.append(prev)
+                count = 1
+            if x == _sentinel:
+                res.append(_sentinel)
+                break
+            prev = x
+    res.pop()
+    return ''.join([str(x) for x in res])
 
 
 if __name__ == '__main__':
