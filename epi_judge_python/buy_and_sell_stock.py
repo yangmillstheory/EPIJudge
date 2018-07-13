902a632457f951ff5e49a0682aba94d714ebe092
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-13 12:06:24 -0700

    max sum subarray

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -1,9 +1,16 @@
 from test_framework import generic_test
 
 
+def find_maximum_subarray(a):
+    prev, max_seen = 0, float('-inf')
+    for x in a:
+        prev = max(prev+x, x)
+        max_seen = max(prev, max_seen)
+    return max(max_seen, 0)
+
+
 def buy_and_sell_stock_once(prices):
-   # TODO - you fill in here.
-    return 0.0
+    return find_maximum_subarray((prices[i]-prices[i-1] for i in range(1, len(prices))))
 
 
 if __name__ == '__main__':
