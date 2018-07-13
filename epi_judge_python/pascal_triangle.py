037b2bc074a24598010ead3ab0ec7b6c6bee806a
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-12 21:35:25 -0700

    pascal's triangle

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -2,8 +2,22 @@ from test_framework import generic_test
 
 
 def generate_pascal_triangle(n):
-    # TODO - you fill in here.
-    return []
+    if not n:
+        return []
+    tri = [[1]]
+    prev = tri[-1]
+    for i in range(1, n):
+        curr = [None]*(i+1)
+        for j in range(i+1):
+            entry = 0
+            if j:
+                entry += prev[j-1]
+            if j < len(prev):
+                entry += prev[j]
+            curr[j] = entry
+        tri.append(curr)
+        prev = curr
+    return tri
 
 
 if __name__ == '__main__':
