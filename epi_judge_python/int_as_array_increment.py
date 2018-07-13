473e7bb7d65b8241e2f77682a04e40fdb6361bad
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-12 21:11:25 -0700

    increment an integer as array

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
 
-def plus_one(A):
-    # TODO - you fill in here.
-    return []
+def plus_one(a):
+    carry = 1
+    for i in range(len(a)-1, -1, -1):
+        digit = a[i]
+        digit += carry
+        if digit >= 10:
+            a[i], carry = 0, 1
+        else:
+            a[i] = digit
+            break
+    else:
+        if carry == 1:
+            a[0] = 1
+            a.append(0)
+    return a
 
 
 if __name__ == '__main__':
