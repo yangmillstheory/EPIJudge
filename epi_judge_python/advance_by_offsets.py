a7512fafae0a76a1929e9daa236766fb0a430b97
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-12 19:52:55 -0700

    DP

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -1,15 +1,27 @@
 from test_framework import generic_test
 
 
-def can_reach_end(a):
+def dp_1(a):
+    # T(n) = O(n^2)
+    # S(n) = O(n)
     n = len(a)
     dp = [True] + [False]*(n-1)
     for j in range(1, n):
-        # it's best to iterate from the right to the left to allow for early termination
         dp[j] = any(dp[i] and i+a[i] >= j for i in range(j-1, -1, -1))
         if not dp[j]:
             return False
     return True
+
+
+def can_reach_end(a):
+    best = float('-inf')
+    for i, jump in enumerate(a):
+        if i > best:
+            return False
+        if i+jump >= len(a)-1:
+            return True
+        best = max(best, i+jump)
+    return best >= len(a)-1
 
 
 if __name__ == '__main__':
