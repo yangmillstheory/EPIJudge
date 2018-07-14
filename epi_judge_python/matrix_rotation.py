3f432fc891e61ca7176a1e6210c0ccd3b9ce661c
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-13 18:58:08 -0700

    in-place rotation of a matrix

--- epi_judge_python/matrix_rotation.py
+++ epi_judge_python/matrix_rotation.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
 
-def rotate_matrix(square_matrix):
-    # TODO - you fill in here.
-    return
+def rotate_matrix(g):
+    '''In-place rotation of a matrix in O(n^2) time.'''
+    n = len(g)
+    if not n:
+        return
+    for d in range(n//2):
+        for k in range(n-1-(2*d)):
+            t1 = g[d+k][-d-1]
+            t2 = g[-d-1][-d-1-k]
+            t3 = g[-d-1-k][d]
+            t4 = g[d][d+k]
+            g[d+k][-d-1] = t4
+            g[-d-1][-d-1-k] = t1
+            g[-d-1-k][d] = t2
+            g[d][d+k] = t3
 
 
 def rotate_matrix_wrapper(square_matrix):
