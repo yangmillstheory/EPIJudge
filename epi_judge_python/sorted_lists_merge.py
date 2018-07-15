29629fdf3b8714302662c6ae0d55c4b05a38d7ed
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-15 16:10:17 -0700

    merge sorted linked lists

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -1,8 +1,20 @@
+from list_node import ListNode
 from test_framework import generic_test
 
 
 def merge_two_sorted_lists(p, q):
-    pass
+    head = prev = ListNode(0)
+    while p and q:
+        if p.data < q.data:
+            prev.next, p = p, p.next
+        else:
+            prev.next, q = q, q.next
+        prev = prev.next
+    if p:
+        prev.next = p
+    elif q:
+        prev.next = q
+    return head.next
 
 
 if __name__ == '__main__':
