7d1ab9e181b0eafe266d5cec1e023e861bbcedfe
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-14 19:37:39 -0700

    reservoir sampling

--- epi_judge_python/online_sampling.py
+++ epi_judge_python/online_sampling.py
@@ -1,5 +1,6 @@
 import functools
-
+import itertools
+from random import randint
 from test_framework import generic_test
 from test_framework.random_sequence_checker import (
     binomial_coefficient, check_sequence_is_uniformly_random,
@@ -8,9 +9,13 @@ from test_framework.test_utils import enable_executor_hook
 
 
 # Assumption: there are at least k elements in the stream.
-def online_random_sample(stream, k):
-    # TODO - you fill in here.
-    return []
+def online_random_sample(it, k):
+    res = [x for x in itertools.islice(it, k)]
+    for j, x in enumerate(it, k):
+        i = randint(0, j)
+        if i < k:
+            res[i] = x
+    return res
 
 
 @enable_executor_hook
