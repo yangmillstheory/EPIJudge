172085e8cf153387c1231306636603515c7ba529
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-15 21:00:39 -0700

    clean

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -11,13 +11,9 @@ def get_kth(head, k):
 
 def reverse_sublist(head, s, f):
     '''Reverse a sublist of a singly-linked list in O(f) time.'''
-    if not head:
-        return
-    if s == 1:
-        prev_s, s_node = ListNode(0), head
-    else:
-        prev_s = get_kth(head, s-1)
-        s_node = prev_s.next
+    dummy = prev_s = ListNode(0, head)
+    prev_s = get_kth(dummy, s)
+    s_node = prev_s.next
     f_node = get_kth(s_node, f-s+1)
     while s_node != f_node:
         next_s_node, s_node.next = s_node.next, f_node.next
@@ -25,7 +21,7 @@ def reverse_sublist(head, s, f):
         s_node = s_node.next
         s_node = next_s_node
     prev_s.next = f_node
-    return prev_s.next if s == 1 else head
+    return dummy.next
 
 
 if __name__ == '__main__':
