d302736c4fab6a93557758245f146a593cbc1cf2
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-16 22:19:23 -0700

    compare IDs

--- epi_judge_python/is_list_cyclic.py
+++ epi_judge_python/is_list_cyclic.py
@@ -5,9 +5,35 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
+def walk(head, n_steps):
+    for _ in range(n_steps):
+        head = head.next
+    return head
+
+
+def get_cycle_length(head):
+    node, n = head, 0
+    while node:
+        n += 1
+        node = node.next
+        if id(node) == id(head):
+            break
+    return n
+
+
 def has_cycle(head):
-    # TODO - you fill in here.
-    return None
+    slow = fast = head
+    while fast and fast.next:
+        slow, fast = slow.next, fast.next.next
+        if id(slow) == id(fast):
+            break
+    if not fast or not fast.next:
+        return None
+    fast = walk(head, get_cycle_length(slow))
+    slow = head
+    while id(fast) != id(slow):
+        slow, fast = slow.next, fast.next
+    return slow
 
 
 @enable_executor_hook
