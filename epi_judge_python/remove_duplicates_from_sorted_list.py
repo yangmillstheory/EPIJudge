d0ac8a84ad92e46133667a3ddc5eb3ccf85b0817
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-17 08:21:30 -0700

    remove dupes from sorted list

--- epi_judge_python/remove_duplicates_from_sorted_list.py
+++ epi_judge_python/remove_duplicates_from_sorted_list.py
@@ -1,9 +1,16 @@
+from list_node import ListNode
 from test_framework import generic_test
 
 
-def remove_duplicates(L):
-    # TODO - you fill in here.
-    return None
+def remove_duplicates(head):
+    dummy = ListNode(0, head)
+    p, q = dummy, dummy.next
+    while p and q:
+        while q.next and q.next.data == q.data:
+            p.next = q.next
+            q = q.next
+        p, q = p.next, q.next
+    return dummy.next
 
 
 if __name__ == '__main__':
