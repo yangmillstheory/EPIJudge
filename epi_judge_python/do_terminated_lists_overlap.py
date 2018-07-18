cc5d45dd0d0681d5980cd3ca9e179b02e5565ec5
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-17 20:02:25 -0700

    test non-cyclic list overlap

--- epi_judge_python/do_terminated_lists_overlap.py
+++ epi_judge_python/do_terminated_lists_overlap.py
@@ -5,9 +5,29 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def overlapping_no_cycle_lists(l0, l1):
-    # TODO - you fill in here.
-    return None
+def length(x):
+    n = 0
+    while x:
+        n += 1
+        x = x.next
+    return n
+
+
+def _overlapping_equal_length_lists(p, q):
+    while p or q:
+        if p is q:
+            return p
+        p = p and p.next
+        q = q and q.next
+
+
+def overlapping_no_cycle_lists(p, q):
+    m, n = length(p), length(q)
+    if m <= n:
+        p, q, m, n = q, p, n, m
+    for _ in range(m-n):
+        p = p.next
+    return _overlapping_equal_length_lists(p, q)
 
 
 @enable_executor_hook
