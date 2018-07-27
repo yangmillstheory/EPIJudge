7cd0ed1b883f7aa1e266badb42868c0bf36318af
Victor Alvarez <v.alvarez312@gmail.com>
2018-07-27 12:59:39 -0700

    rabin karp

--- epi_judge_python/substring_match.py
+++ epi_judge_python/substring_match.py
@@ -1,9 +1,25 @@
+from functools import reduce
 from test_framework import generic_test
 
 
 def rabin_karp(t, s):
-    # TODO - you fill in here.
-    return 0
+    m, n = len(t), len(s)
+    if m < n:
+        return -1
+    elif m == n:
+        return 0 if t == s else -1
+    base = 26
+    t_hash = reduce(lambda h, ch: base*h + ord(ch), t[:n], 0)
+    s_hash = reduce(lambda h, ch: base*h + ord(ch), s, 0)
+    for i in range(m-n):
+        if t_hash == s_hash and t[i:i+n] == s:
+            return i
+        t_hash -= pow(base, n-1)*ord(t[i])
+        t_hash *= base
+        t_hash += ord(t[i+n])
+    if t_hash == s_hash and t[m-n:] == s:
+        return m-n
+    return -1
 
 
 if __name__ == '__main__':
