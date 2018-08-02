cbef7684788e17d64bc10e3d1d2b3539a7caa6dc
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-01 22:52:11 -0700

    fuck.

--- epi_judge_python/max_of_sliding_window.py
+++ epi_judge_python/max_of_sliding_window.py
@@ -1,7 +1,29 @@
 import functools
-
+from collections import deque
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
+
+
+class QueueWithMax(deque):
+    def __init__(self):
+        self._next_maxes = deque()
+
+    def enqueue(self, x):
+        self.append(x)
+        while self._next_maxes and self._next_maxes[-1] < x:
+            self._next_maxes.pop()
+        self._next_maxes.append(x)
+
+    def dequeue(self):
+        x = self.popleft()
+        if x == self.max():
+            self._next_maxes.popleft()
+        return x
+
+    def max(self):
+        if not self._next_maxes:
+            raise ValueError('No max to return!')
+        return self._next_maxes[0]
 
 
 class TrafficElement:
@@ -10,9 +32,14 @@ class TrafficElement:
         self.volume = volume
 
 
-def calculate_traffic_volumes(A, w):
-    # TODO - you fill in here.
-    return []
+def calculate_traffic_volumes(traffic, w):
+    q, res = QueueWithMax(), []
+    for t in traffic:
+        while q and t.time - q[0][1] > w:
+            q.dequeue()
+        q.enqueue((t.volume, t.time))
+        res.append(TrafficElement(t.time, q.max()[0]))
+    return res
 
 
 @enable_executor_hook
