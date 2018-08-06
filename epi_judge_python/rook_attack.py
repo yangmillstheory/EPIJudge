fb521056c121627f1680f18409e1488d41be9dde
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-05 17:15:15 -0700

    rook attack

--- epi_judge_python/rook_attack.py
+++ epi_judge_python/rook_attack.py
@@ -3,9 +3,35 @@ import copy
 from test_framework import generic_test
 
 
-def rook_attack(A):
-    # TODO - you fill in here.
-    return
+def rook_attack(g):
+    m, n = len(g), len(g[0])
+    row_1_0 = any(x == 0 for x in g[0])
+    col_1_0 = any(g[i][0] == 0 for i in range(m))
+    for i in range(1, m):
+        for j in range(1, n):
+            if g[i][j] == 0:
+                g[0][j] = g[i][0] = 0
+
+    def set_row_zero(i):
+        for j in range(1, n):
+            g[i][j] = 0
+
+    def set_col_zero(j):
+        for i in range(1, m):
+            g[i][j] = 0
+
+    for i in range(1, m):
+        if g[i][0] == 0:
+            set_row_zero(i)
+    for j in range(1, n):
+        if g[0][j] == 0:
+            set_col_zero(j)
+    if row_1_0:
+        set_row_zero(0)
+    if col_1_0:
+        set_col_zero(0)
+    if row_1_0 or col_1_0:
+        g[0][0] = 0
 
 
 def rook_attack_wrapper(A):
