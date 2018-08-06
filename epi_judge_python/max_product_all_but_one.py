f19def22eac89c420688a8c5d08596195700edbb
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-05 17:51:29 -0700

    a problem

--- epi_judge_python/max_product_all_but_one.py
+++ epi_judge_python/max_product_all_but_one.py
@@ -1,9 +1,16 @@
 from test_framework import generic_test
 
 
-def find_biggest_n_minus_one_product(A):
-    # TODO - you fill in here.
-    return 0
+def find_biggest_n_minus_one_product(a):
+    n = len(a)
+    suf = [1]*n
+    for j in range(n-2, -1, -1):
+        suf[j] = suf[j+1]*a[j+1]
+    pre, res = 1, 0
+    for i, x in enumerate(a):
+        res = max(res, pre*suf[i])
+        pre *= x
+    return res
 
 
 if __name__ == '__main__':
