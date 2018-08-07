a17bd3f19688c8de4c15d537a258311d1b21df84
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-06 21:28:52 -0700

    do it

--- epi_judge_python/longest_increasing_subarray.py
+++ epi_judge_python/longest_increasing_subarray.py
@@ -5,9 +5,19 @@ from test_framework import generic_test
 Subarray = collections.namedtuple('Subarray', ('start', 'end'))
 
 
-def find_longest_increasing_subarray(A):
-    # TODO - you fill in here.
-    return Subarray(0, 0)
+def find_longest_increasing_subarray(a):
+    s = Subarray(0, 0)
+    i, n = 0, len(a)
+    while i < n:
+        j = i
+        while j < n:
+            if j+1 == n or a[j] >= a[j+1]:
+                break
+            j += 1
+        if j-i >= s[1]-s[0]:
+            s = Subarray(i, j)
+        i = j+1
+    return s
 
 
 def find_longest_increasing_subarray_wrapper(A):
