e320dc14295bcff1c732903657e0f5ec09abed02
Victor Alvarez <v.alvarez312@gmail.com>
2018-08-12 11:50:41 -0700

    skyline prob.

--- epi_judge_python/drawing_skyline.py
+++ epi_judge_python/drawing_skyline.py
@@ -1,3 +1,4 @@
+import operator
 import collections
 import functools
 
@@ -7,9 +8,21 @@ from test_framework.test_utils import enable_executor_hook
 Rectangle = collections.namedtuple('Rectangle', ('left', 'right', 'height'))
 
 
-def compute_skyline(buildings):
-    # TODO - you fill in here.
-    return []
+def compute_skyline(a):
+    min_l = min(a, key=operator.attrgetter('left')).left
+    max_r = max(a, key=operator.attrgetter('right')).right
+    h = [0]*(max_r - min_l + 1)
+    for l, r, _h in a:
+        for i in range(l, r+1):
+            h[i-min_l] = max(h[i-min_l], _h)
+    sl = []
+    left = 0
+    for i, _h in enumerate(h):
+        if i and _h != h[i-1]:
+            sl.append(Rectangle(min_l+left, min_l+i-1, h[i-1]))
+            left = i
+    sl.append(Rectangle(min_l+left, max_r, h[-1]))
+    return sl
 
 
 @enable_executor_hook
